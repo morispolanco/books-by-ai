2e8b07e543b1c37edfb2602d171df7a64e82a4ab
Tim Zadorozhny <tzador@gmail.com>
2023-08-06 02:49:17 +0200

    intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,24 +9,40 @@
 
 <h1>Library of books written by AI</h1>
 
+<p>
+  Welcome :) Here, you can read and <b>create new books</b> from scratch, tailored to your interests,
+  with the help of ChatGPT.
+</p>
+<p>
+  Just provide a title and optionally additional instructions regarding the content of the book, and
+  book overview plus table of contents will be generated for you.
+</p>
+<p>Chapter and their section content is generated on demand, once you start reading the book.</p>
+<p>
+  If you are not satisfied with the generated content, or need additional information or
+  clarifications, the system allows you to <b>chat with the content</b>, in order to receive
+  additional information tailored to your interests.
+</p>
+<p>Currently all the books are public, and listed on the homepage, sorted into categories.</p>
+
 <p><a href="/new"><button class="btn-success">Create New Book</button></a></p>
 
-<div class="categories">
+<div class="categories paper">
   {#each categories as category}
     <span class="category"><a href={`#${slugit(category)}`}>{category}</a></span>
   {/each}
 </div>
 
 {#each categories as category}
-  <h3 id={slugit(category)}>{category}</h3>
+  <h4 id={slugit(category)}>{category}</h4>
 
-  <ul>
+  <ol>
     {#each data.books.filter((book) => book.category === category) as book}
       <li>
         <a href={`/book/${book.slug}/${book.id}`}>{book.title}</a>
       </li>
     {/each}
-  </ul>
+  </ol>
 {/each}
 
 <style>
@@ -36,5 +52,9 @@
     column-gap: 1rem;
     row-gap: 0.25rem;
     justify-content: space-between;
+  }
+
+  b {
+    text-decoration: underline;
   }
 </style>
